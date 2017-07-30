a3b7cce4e4f02b33019687a27c63d0987cb39bda
fazaton <fazaton@gmail.com>
2017-07-30 13:15:54 -0500

    commit

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -1,5 +1,6 @@
 class KittensController < ApplicationController
 	def new
+		@kitten = Kitten.new
 	end
 
 	def index
@@ -11,9 +12,30 @@ class KittensController < ApplicationController
 	end
 
 	def create
+		Kitten.create(kitten_params)
+		redirect_to root_path
 	end
 
-	def update 
+	def edit 
+		@kitten = Kitten.find(params[:id])
+	end
+
+	def update
+		@kitten = Kitten.find(params[:id])
+		@kitten.update_attributes(kitten_params)
+		redirect_to root_path
+	end
+
+	def destroy
+		@kitten = Kitten.find(params[:id])
+		@kitten.destroy
+		redirect_to root_path
+	end
+
+	private
+
+	def kitten_params
+		params.require(:kitten).permit(:name, :age, :cuteness, :softness)
 	end
 
 end
